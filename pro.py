17b2e337f68a37d343c1fc32bd66d8a9022e51f5
Ar-Soc <mohammedameen9011@gmail.com>
2023-08-13 13:38:40 +0530

    First version

---
+++ pro.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pickle as pk
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+import pandas as pd
+
+model = pk.load(open(r"abc.pickle", "rb"))
+
+st.title("Life Expectancy Predictor")
+
+Birth_Rate = st.text_input("Birth Rate")
+Fertility_Rate = st.text_input("Fertility Rate")
+Infant_mortality = st.text_input("Infant mortality")
+Maternal_mortality_ratio = st.text_input("Maternal mortality ratio")
+Physicians_per_thousand = st.text_input("Physicians per thousand")
+
+
+k = st.button("Predict")
+
+
+if k:
+    x = pd.DataFrame([[Birth_Rate,Fertility_Rate,Infant_mortality,Maternal_mortality_ratio,Physicians_per_thousand]])
+    x.columns = ["Birth Rate","Fertility Rate","Infant mortality","Maternal mortality ratio","Physicians per thousand"]
+    prediction = model.predict(x)
+    st.markdown(prediction)
+
